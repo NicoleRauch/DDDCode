efb079a699182e224b7cd7b3033a8eb78d8582da
Nicole Rauch <info@nicole-rauch.de>
2023-06-03 22:24:54 +0200

    Diese Methode sollten wir auch im Interface exponieren.

--- src/main/java/com/example/ddd_es/lager/domainLoesung1/Repository.java
+++ src/main/java/com/example/ddd_es/lager/domainLoesung1/Repository.java
@@ -1,9 +1,11 @@
 package com.example.ddd_es.lager.domainLoesung1;
 
 import com.example.ddd_es.lager.domainLoesung1.Event;
+import com.google.common.collect.ImmutableList;
 
 public interface Repository {
 
     void storeEvent(Event event);
 
+    public ImmutableList<Event> events();
 }
